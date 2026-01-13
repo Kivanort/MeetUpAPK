f7d8fb1484ba2cf28dbb6408de85a6806ac2249e
Kivanort <152086911+Kivanort@users.noreply.github.com>
2026-01-13 23:23:08 +0300

    Create metro.config.js

---
+++ metro.config.js
@@ -0,0 +1,17 @@
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+const defaultConfig = getDefaultConfig(__dirname);
+
+const config = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: true,
+      },
+    }),
+  },
+  resetCache: true,
+};
+
+module.exports = mergeConfig(defaultConfig, config);
